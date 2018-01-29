1dbf7de9937c085562f260ec779887610564480b
Wilfrid FOULON <wfoulon@e1r10p15.42.fr>
2018-01-29 16:06:17 +0100

    level4-5

--- rendu/4-sort_list/sort_list.c
+++ rendu/4-sort_list/sort_list.c
@@ -5,98 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: wfoulon <wfoulon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/15 11:06:55 by wfoulon           #+#    #+#             */
-/*   Updated: 2018/01/15 11:07:11 by wfoulon          ###   ########.fr       */
+/*   Created: 2018/01/29 15:58:28 by wfoulon           #+#    #+#             */
+/*   Updated: 2018/01/29 16:03:14 by wfoulon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "list.h"
 
-static int	lst_len(t_list *lst)
+t_list		*sort_list(t_list* lst, int(*cmp)(int, int))
 {
-	int i;
+	int		overflow;
+	t_list	*tmp;
 
-	i = 0;
-	while (lst)
+	tmp = lst;
+	while (lst->next != NULL)
 	{
-		i++;
-		lst = lst->next;
-	}
-	return (i);
-}
-
-static void convert_to_arr(t_list **arr, t_list *lst)
-{
-	int i;
-
-	i = 0;
-	while (lst)
-	{
-		arr[i] = lst;
-		lst = lst->next;
-		i++;
-	}
-	arr[i] = 0;
-}
-
-static void arr_sort(t_list **arr, int len, int (*cmp)(int, int))
-{
-	int i;
-	int j;
-	t_list *tmp;
-
-	j = 0;
-	while (j < len - 1)
-	{
-		i = 0;
-		while (i < len - j - 1)
+		if (((*cmp)(lst->data, lst->next->data)) == 0)
 		{
-			if (cmp(arr[i]->data, arr[i + 1]->data) > 0)
-			{
-				tmp = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = tmp;
-			}
-			i++;
+			overflow = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = overflow;
+			lst = tmp;
 		}
-		j++;
+		else
+			lst = lst->next;
 	}
-}
-
-static t_list *lst_push(t_list *lst, t_list *elem)
-{
-	t_list *save;
-
-	save = lst;
-	elem->next = lst;
-	save = elem;
-	return (save);
-}
-
-static t_list *convert_to_lst(t_list **arr)
-{
-	t_list *save;
-	int i;
-
-	save = 0;
-	i = 0;
-	while (arr[i])
-	{
-		save = lst_push(save, arr[i]);
-		arr++;
-	}
-	return (save);
-}
-
-t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
-{
-	int len;
-	t_list *arr[100000];
-	t_list *new_lst;
-
-	len = lst_len(lst);
-	convert_to_arr(arr, lst);
-	arr_sort(arr, len, cmp);
-	new_lst = convert_to_lst(arr);
-	return (new_lst);
+	lst = tmp;
+	return (lst);
 }
